6438f6f611f24511643a920cd47dac7c5bd0f4a9
John Kraczek <John.kraczek@gmail.com>
2024-11-01 08:05:14 -0600

    Require BuddyBoss Platform to be activated

--- ydtb-business-card.php
+++ ydtb-business-card.php
@@ -12,6 +12,22 @@
  */
 require_once __DIR__ . '/vendor/autoload.php';
 
+/**
+ * Require BuddyBoss Platform
+ */
+if (!defined('BP_PLATFORM_VERSION')) {
+    add_action('admin_notices', 'YDTBC_Platform_install_bb_platform_notice');
+    add_action('network_admin_notices', 'YDTBC_Platform_install_bb_platform_notice');
+    return;
+}
+
+function YDTBC_Platform_install_bb_platform_notice()
+{
+    echo '<div class="error fade"><p>';
+    _e('<strong>YDTB Business Card</strong></a> requires the BuddyBoss Platform plugin to work. Please <a href="https://buddyboss.com/platform/" target="_blank">install BuddyBoss Platform</a> first.', 'ydtb');
+    echo '</p></div>';
+}
+
 $ydtbbc = new YDTBC\Providers\BusinessCardServiceProvider;
 $ydtbbc->register();
 
